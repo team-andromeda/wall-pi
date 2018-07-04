f14b32b66484dec5b14ad32cce100168250ba961
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-07-04 10:14:07 +0200

    Config schema

--- src/app/_models/config.model.ts
+++ src/app/_models/config.model.ts
@@ -1,7 +1,18 @@
+export interface Location {
+    name: string;
+    locationString: string;
+}
+
+export interface Room {
+    id: string;
+    locations: Location[];
+}
+
 export interface Config {
     authOptions: {
         url: string;
         client_id: string;
         redirect_uri: string;
     };
+    rooms: Room[];
 }
