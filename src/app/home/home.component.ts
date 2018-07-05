faa363e470b8a9373250e41cc478544238005975
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-07-05 09:44:47 +0200

    Refresh data every 30 seconds

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActiveDirectoryService } from '../_services/active-directory.service';
 import * as Transform from '../../assets/js/transform.js';
 import { ConfigService } from 'src/app/_services/config.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,17 +14,31 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private activeDirService: ActiveDirectoryService,
-    private configService: ConfigService
-  ) {}
+    private configService: ConfigService,
+    private router: Router
+  ) {
+    window.setInterval(() => {
+      console.log('Refreshing...');
+      this.fetchData();
+    }, 30 * 1000);
+  }
 
   ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
     this.activeDirService
       .getCalendar(this.configService.getRoomId())
       .subscribe((res) => {
         // this.data = res;
         // TODO(egeldenhuys): Send time and venue paramater
         console.log(res);
-        Transform.run(res, this.configService.getRoomName(), this.activeDirService.formatDate(new Date()));
+        Transform.run(
+          res,
+          this.configService.getRoomName(),
+          this.activeDirService.formatDate(new Date())
+        );
       });
   }
 
